cde297e3f0c31487486664c878fa2e690168adf8
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-03-03 14:40:20 -0300

    elimino nav de bootstap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Main from "./components/Main";
-import Navbarr from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbarr/>
+        <Navbar/>
         <Main/>
         </React.Fragment>
     )
